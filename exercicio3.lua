87c55474639037653f0fdbbe697bf8652ea04f33
Twiinsengod <crufabio@gmail.com>
2023-06-22 20:36:14 -0300

    exercicio3

---
+++ exercicio3.lua
@@ -0,0 +1,7 @@
+-- Leia dois números e mostre a soma entre eles
+
+print("Digite o 1o valor:")
+local valor1 = io.read()
+print("Digite o 2o valor:")
+local valor2 = io.read()
+print("A soma dos valores é: ".. valor1 + valor2)
