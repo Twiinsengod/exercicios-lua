6e1982120ca091df1dfeb1d6ec2b38722b8906a3
Twiinsengod <crufabio@gmail.com>
2023-06-22 20:14:01 -0300

    exercicio1

---
+++ exercicio1.lua
@@ -0,0 +1,4 @@
+-- Escreva "Olá, Mundo!" na tela.
+local olaMundo = "Olá Mundo"  --é usando para declarar a variável
+print(olaMundo) --string: é o texto que aparece (exemplos: ç, acento e etc). usar sempre ""
+
