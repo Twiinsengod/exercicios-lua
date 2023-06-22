e5efc2d17c509b1317c68a3ae2700acae7065e73
Twiinsengod <crufabio@gmail.com>
2023-06-22 20:27:05 -0300

    exercicio 2

---
+++ exercicio2.lua
@@ -0,0 +1,7 @@
+-- Leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+
+print("Digite o seu nome:")
+local nome = io.read()
+
+-- Exibe o nome lido
+print("O nome digitado foi: " .. nome)
